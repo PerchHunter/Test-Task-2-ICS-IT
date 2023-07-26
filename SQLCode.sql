8e52fa7fc647e2bc18f69fed91fc4e9c1e91223d
Sergey Feoktistov <86980313+PerchHunter@users.noreply.github.com>
2023-07-26 14:51:36 +0300

    Returned the file to its original form, added a few comments about the inconsistency with the established code style

--- SQLCode.sql
+++ SQLCode.sql
@@ -1,33 +1,38 @@
--- 1. Должен быть комментарий в шапке объекта
+/*
+	1. Должен быть комментарий в шапке объекта.
+	Нужно писать "create or alter", чтобы повторное выполнение скрипта не приводило к возникновению ошибки.
+	Если не ошибаюсь, если один параметр, то его можно не переносить на следующую строку
+*/
 create procedure syn.usp_ImportFileCustomerSeasonal
 	@ID_Record int
 as
 set nocount on
 begin
-
 	-- 2. Все переменные должны задаваться в одном объявлении
 	-- 3. Рекомендуется при объявлении типов не использовать длину поля max
-	declare 
-		@RowCount int = (select count(*) from syn.SA_CustomerSeasonal)
-		,@ErrorMessage varchar(1000)
+	declare @RowCount int = (select count(*) from syn.SA_CustomerSeasonal)
+	declare @ErrorMessage varchar(max)
 
 	-- 4. Коммментарий должен быть на одном уровне с кодом, к которому он относится
-	-- Проверка на корректность загрузки
+-- Проверка на корректность загрузки
 	if not exists (
-		-- 5. Внутри скобок код должен быть смещён на один таб
-		select 1
-		-- 6. Не правильный алиас. Должен быть "imf". Не "if", т.к. это системное слово
-		from syn.ImportFile as imf
-		where imf.ID = @ID_Record
-			and imf.FlagLoaded = cast(1 as bit)
+	-- 5. Внутри скобок код должен быть смещён на один таб
+	select 1
+	-- 6. Не правильный алиас. Должен быть "imf". Не "if", т.к. это системное слово
+	from syn.ImportFile as f
+	where f.ID = @ID_Record
+		and f.FlagLoaded = cast(1 as bit)
 	)
+	-- 7. if и else с begin/end должны быть на одном уровне
 		begin
 			set @ErrorMessage = 'Ошибка при загрузке файла, проверьте корректность данных'
 
 			raiserror(@ErrorMessage, 3, 1)
+			-- 8. Должна быть пустая строка перед "return"
 			return
 		end
 
+	-- 9. CREATE TABLE ловеркейсом, должен быть пробел между скобкой и названием таблицы
 	CREATE TABLE #ProcessedRows(ActionType varchar(255), ID int)
 	
 	--Чтение из слоя временных данных
@@ -35,64 +40,55 @@ begin
 		cc.ID as ID_dbo_Customer
 		,cst.ID as ID_CustomerSystemType
 		,s.ID as ID_Season
-		,cast(cs.DateBegin as date) as DateBegin
-		,cast(cs.DateEnd as date) as DateEnd
+		,cast(sa.DateBegin as date) as DateBegin
+		,cast(sa.DateEnd as date) as DateEnd
 		,cd.ID as ID_dbo_CustomerDistributor
-		,cast(isnull(cs.FlagActive, 0) as bit) as FlagActive
+		,cast(isnull(sa.FlagActive, 0) as bit) as FlagActive
 	into #CustomerSeasonal
-	-- 7. Пропущено "as". syn.SA_CustomerSeasonal as cs
-	-- 8. Как я понимаю, во всём запросе вместо алиаса "sa" должен быть алиас "cs". 
-	-- Для таблицы syn.SA_CustomerSeasonal правильный алиас "cs"
-	from syn.SA_CustomerSeasonal as cs
-		join dbo.Customer as cc on cc.UID_DS = cs.UID_DS_Customer
+	-- 10. Пропущено "as". syn.SA_CustomerSeasonal as cs
+	-- 11. Во всём этом запросе и запросе ниже по коду вместо алиаса "sa" должен быть алиас "cs"
+	from syn.SA_CustomerSeasonal cs
+		join dbo.Customer as cc on cc.UID_DS = sa.UID_DS_Customer
 			and cc.ID_mapping_DataSource = 1
-		join dbo.Season as s on s.Name = cs.Season
-		join dbo.Customer as cd on cd.UID_DS = cs.UID_DS_CustomerDistributor
+		join dbo.Season as s on s.Name = sa.Season
+		join dbo.Customer as cd on cd.UID_DS = sa.UID_DS_CustomerDistributor
 			and cd.ID_mapping_DataSource = 1
-		-- 9. Сперва указываем поле присоединяемой таблицы	
-		join syn.CustomerSystemType as cst on cst.Name = cs.CustomerSystemType
-	where try_cast(cs.DateBegin as date) is not null
-		and try_cast(cs.DateEnd as date) is not null
-		and try_cast(isnull(cs.FlagActive, 0) as bit) is not null
+		-- 12. Сперва указываем поле присоединяемой таблицы
+		join syn.CustomerSystemType as cst on sa.CustomerSystemType = cst.Name
+	where try_cast(sa.DateBegin as date) is not null
+		and try_cast(sa.DateEnd as date) is not null
+		and try_cast(isnull(sa.FlagActive, 0) as bit) is not null
 
 	-- Определяем некорректные записи
 	-- Добавляем причину, по которой запись считается некорректной
 	select
-		-- 10. Снова в запросе везде вместо алиаса "cs" написан "sa". Нужно исправлять
-		cs.*
+		sa.*
 		,case
-			-- 11. "then" везде должен быть перенесён на следующую строку и сдвинут на один таб
-			when cc.ID is null 
-				then 'UID клиента отсутствует в справочнике "Клиент"'
-			when cd.ID is null 
-				then 'UID дистрибьютора отсутствует в справочнике "Клиент"'
-			when s.ID is null 
-				then 'Сезон отсутствует в справочнике "Сезон"'
-			when cst.ID is null 
-				then 'Тип клиента в справочнике "Тип клиента"'
-			when try_cast(cs.DateBegin as date) is null 
-				then 'Невозможно определить Дату начала'
-			when try_cast(cs.DateEnd as date) is null 
-				then 'Невозможно определить Дату начала'
-			when try_cast(isnull(cs.FlagActive, 0) as bit) is null 
-				then 'Невозможно определить Активность'
+			-- 13. "then" везде должен быть перенесён на следующую строку и сдвинут на один таб
+			when cc.ID is null then 'UID клиента отсутствует в справочнике "Клиент"'
+			when cd.ID is null then 'UID дистрибьютора отсутствует в справочнике "Клиент"'
+			when s.ID is null then 'Сезон отсутствует в справочнике "Сезон"'
+			when cst.ID is null then 'Тип клиента в справочнике "Тип клиента"'
+			when try_cast(sa.DateBegin as date) is null then 'Невозможно определить Дату начала'
+			when try_cast(sa.DateEnd as date) is null then 'Невозможно определить Дату начала'
+			when try_cast(isnull(sa.FlagActive, 0) as bit) is null then 'Невозможно определить Активность'
 		end as Reason
 	into #BadInsertedRows
 	from syn.SA_CustomerSeasonal as cs
-		-- 12. Все виды join пишутся с одним отступом. При сдвиге также нужно будет сдвинуть и "and'ы"
-		left join dbo.Customer as cc on cc.UID_DS = cs.UID_DS_Customer
-			and cc.ID_mapping_DataSource = 1
-	-- 13. "and" не был перенесён на следующую строку
-		left join dbo.Customer as cd on cd.UID_DS = cs.UID_DS_CustomerDistributor 
-			and cd.ID_mapping_DataSource = 1
-		left join dbo.Season as s on s.Name = cs.Season
-		left join syn.CustomerSystemType as cst on cst.Name = cs.CustomerSystemType
+	-- 13. Все виды join пишутся с одним отступом. При сдвиге также нужно будет сдвинуть и "and'ы"
+	left join dbo.Customer as cc on cc.UID_DS = sa.UID_DS_Customer
+		and cc.ID_mapping_DataSource = 1
+	-- 14. "and" не был перенесён на следующую строку
+	left join dbo.Customer as cd on cd.UID_DS = sa.UID_DS_CustomerDistributor and cd.ID_mapping_DataSource = 1
+	left join dbo.Season as s on s.Name = sa.Season
+	left join syn.CustomerSystemType as cst on cst.Name = sa.CustomerSystemType
 	where cc.ID is null
 		or cd.ID is null
 		or s.ID is null
 		or cst.ID is null
-		or try_cast(cs.DateBegin as date) is null
-		or try_cast(cs.DateEnd as date) is null
-		or try_cast(isnull(cs.FlagActive, 0) as bit) is null
--- 14. Две пустые строки перед "end" лишние
+		or try_cast(sa.DateBegin as date) is null
+		or try_cast(sa.DateEnd as date) is null
+		or try_cast(isnull(sa.FlagActive, 0) as bit) is null
+
+-- 15. Две пустые строки перед "end" лишние
 end
